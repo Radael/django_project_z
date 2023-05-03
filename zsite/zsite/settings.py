c600214a355fb4a58f8c1ad56a381f0c9206316e
Radael <iskander.nurislamov@gmail.ru>
2023-05-03 13:14:54 +0500

    fixed gitignore

--- zsite/zsite/settings.py
+++ zsite/zsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from local_settings import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--9o*ehp0fd=1-xvp8dke)d+e2*_xv6^%+*vul4qmoj#%imwoso'
+SECRET_KEY = SECRET_KEY  # what the fuck???
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
